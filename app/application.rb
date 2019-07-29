4a11c4b3d2c493ca01bdec41fe6b7955110589cb
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-29 17:34:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,21 +9,17 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      #item_match = @@items.detect{|i| i.name == item_name}
-       if item_match = @@items.detect{|i| i.name == item_name}
-        resp.write item_match.price
-       else
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else 
         resp.status = 400
         resp.write "Item not found"
-       end 
-      
-    else 
-      resp.status = 404
+      end
+    else
+      resp.status=404
       resp.write "Route not found"
-    end 
+    end
     resp.finish
-  end 
-  
+  end
 
- 
-end 
+end
