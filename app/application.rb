34c5c23c156d1535ee0f915e6ae5d9f162717fc9
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-29 17:34:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,10 @@
-require 'pry'
+class Application
 
-class Application 
+  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
   def call(env)
-    resp = Rack::Response.new 
+    resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
-    @@items = [Item.new("Mango", 2), Item.new("Pear", 3)]
-    
+
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if item =@@items.find{|i| i.name == item_name}
